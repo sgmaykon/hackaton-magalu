b82967e10f36f1d5887ece014105454c7f44c0d0
guugimeness <gucrome1@gmail.com>
2024-11-01 03:42:51 -0300

    feat: deploy api-key

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,9 @@ terraform {
 }
 
 provider "mgc" {
-  alias  = "nordeste"
-  region = "br-ne1"
+  alias   = "nordeste"
+  region  = "br-ne1"
+  api_key = "83e27de5-9c8e-4210-b742-f9a788dbd16c"
 }
 
 resource "mgc_virtual_machine_instances" "vm-nordeste" {
